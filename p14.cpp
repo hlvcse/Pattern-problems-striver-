215e9eae121acc774e26325415fec884e6d3b17b
Hanumat Lal Vishwakarma <106891939+hlvcse@users.noreply.github.com>
2022-08-26 22:59:13 -0800

    Create p14.cpp

---
+++ p14.cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   
+    for(int i=1;i<=5;i++)
+    {  
+        char ch='A';
+        for(int j=1;j<=i;j++)
+        {  
+           cout<<ch++;
+         }
+          cout<<"\n";
+    }
+       
+    return 0;
+}
